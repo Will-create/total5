ffeff8622d322747ae08a3a044f70f42c047387d
Louis Bertson <louisbertson@gmail.com>
2025-04-20 18:05:23 +0000

    Added cargo app

---
+++ testapp/target/debug/deps/testapp-02d9ffdf46014593.d
@@ -0,0 +1,5 @@
+/Users/louisbertson/Desktop/rust/total5/testapp/target/debug/deps/testapp-02d9ffdf46014593: src/main.rs
+
+/Users/louisbertson/Desktop/rust/total5/testapp/target/debug/deps/testapp-02d9ffdf46014593.d: src/main.rs
+
+src/main.rs:
---
+++ testapp/target/debug/testapp.d
@@ -0,0 +1,1 @@
+/Users/louisbertson/Desktop/rust/total5/testapp/target/debug/testapp: /Users/louisbertson/Desktop/rust/total5/testapp/src/main.rs
